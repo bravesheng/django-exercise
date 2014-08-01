fc79afe5efd9a624b2e8f7e218cdfbef4d158c79
bravesheng <bravesheng@gmail.com>
2014-08-01 19:14:40 +0800

    chapter04 templates part1

--- article/views.py
+++ article/views.py
@@ -33,11 +33,12 @@ def create(request):
     form = ArticleForm()
     return render(request, 'create_article.html', {'form': form})
 
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 import datetime
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render_to_response('current_datetime.html', {'current_date': now})
 
 from django.http import Http404
 
